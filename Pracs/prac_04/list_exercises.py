9e2eb892e3fc2c1a276cdaace4697b40712d17ff
LazorLynx <lazorlynx@gmail.com>
2022-10-05 20:18:32 +1000

    Add subject_reader.py program and refactor subject_reader.py

---
+++ Pracs/prac_04/list_exercises.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 - List warmup - Chloe Laing
+
+output vvv
+
+Number: 5
+   Number: 20
+   Number: 1
+   Number: 2
+   Number: 3
+   The first number is 5
+   The last number is 3
+   The smallest number is 1
+   The largest number is 20
+   The average of the numbers is 6.2
+
+"""
