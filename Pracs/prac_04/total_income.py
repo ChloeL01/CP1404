50272dee938f828db61cdf651fa1559c9acfbc01
LazorLynx <lazorlynx@gmail.com>
2022-10-05 18:58:54 +1000

    Add lists_warmup.py and total_income.py programs to the Week 4 folder

---
+++ Pracs/prac_04/total_income.py
@@ -0,0 +1,34 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        # income = float(input("Enter income for month " + str(month) + ": "))
+        incomes.append(income)
+
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, number_of_months + 1):
+        income = calculate_income(incomes, month)
+        total = total + income
+        print("Month {:2} - Income: ${:10.2f}       Total: ${:10.2f}".format(month, income, total))
+
+
+def calculate_income(incomes, month):
+    income = incomes[month - 1]
+    return income
+
+
+main()
