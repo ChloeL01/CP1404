ba0043cca0153a6fdccee5a4ca02d236a0a8d5e0
LazorLynx <lazorlynx@gmail.com>
2022-11-05 00:28:08 +1000

    Start project management exercise

--- Pracs/prac_07/project.py
+++ Pracs/prac_07/project.py
@@ -1,11 +1,24 @@
 """CP1404/CP5632 Project class"""
 
+COMPLETION_PERCENTAGE = 100
+
 
 class Project:
     """Represent a project object."""
 
-    def __init__(self):
-        pass
+    def __init__(self, name, start_date, priority, cost, completion_percentage):
+        """Initialise."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion_percentage = completion_percentage
 
     def __str__(self):
-        pass
+        """Return a string representation."""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost:,.2f}, " \
+               f"completion: {self.completion_percentage}%"
+
+    def is_complete(self):
+        """Return True if percentage is less than 100% done."""
+        return self.completion_percentage < COMPLETION_PERCENTAGE
--- Pracs/prac_07/project_management.py
+++ Pracs/prac_07/project_management.py
@@ -1,8 +1,64 @@
 """CP1404/CP5632 Practical project management"""
+from Pracs.prac_07.project import Project
+
+MENU = "- (L)oad projects \n" \
+       "- (S)ave projects \n" \
+       "- (D)isplay Projects \n" \
+       "- (F)ilter objects \n" \
+       "- (A)dd new project \n" \
+       "- (U)pdate project \n" \
+       "- (Q)uit"
+
+FILENAME = "projects.txt"
 
 
 def main():
-    pass
+    """Project manager."""
+    projects = get_data(FILENAME)
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print("L")
+        elif choice == "S":
+            print("S")
+        elif choice == "D":
+            display_projects(projects)
+        elif choice == "F":
+            print("F")
+        elif choice == "A":
+            print("A")
+        elif choice == "U":
+            print("U")
+        else:
+            print("Invalid input")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you for using custom-built project management software.")
+
+
+def get_data(filename):
+    """Get data from file formatted like 'Name	Start Date	Priority	Cost Estimate	Completion Percentage'."""
+    projects = []
+    with open(filename, "r") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split("\t")
+            project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+            projects.append(project)
+    return projects
+
+
+def display_projects(projects):
+    """Display projects from the projects list"""
+    print("Incomplete projects:")
+    for project in projects:
+        if project.is_complete():
+            print(f"  {project}")
+    print("Complete projects:")
+    for project in projects:
+        if not project.is_complete():
+            print(f"  {project}")
 
 
 if __name__ == '__main__':
